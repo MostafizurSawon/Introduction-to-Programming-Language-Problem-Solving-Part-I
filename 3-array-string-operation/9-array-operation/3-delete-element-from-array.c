6ef901755eb763fc653618471b7569d6e8f2d5fd
unknown <mostafizur1102@gmail.com>
2023-04-16 13:40:57 +0600

    array insertion

---
+++ 3-array-string-operation/9-array-operation/3-delete-element-from-array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,m;
+    scanf("%d",&n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&m);
+    for (int i = m-1; i < n; i++)
+    {
+        arr[i] = arr[i+1];
+    }
+    arr[n-1] = 0;
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    
+    
+    
+   
+    return 0;
+}
